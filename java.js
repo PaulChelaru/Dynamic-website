ffef5071a3647efb1d90430fed53e661fdfa32e7
Paul Chelaru <paul5175@stud.kea.dk>
2018-02-22 00:19:54 +0100

    Cat de cat prima pagina

--- java.js
+++ java.js
@@ -3,32 +3,29 @@
 
 const template = document.querySelector('template').content;
 const main = document.querySelector('main');
-const link = "https://raw.githubusercontent.com/lanasladovic/data/master/data.json";
+const link = "https://raw.githubusercontent.com/PaulChelaru/Dynamic-website/master/data.json?token=AeqwT8ebbeO9NuqQkaUh5pE9Sa2YtPqjks5alwsxwA%3D%3D";
 
-import data from './data.json';
-console.log(data);
+//for (let i = 1; i < 4; i++) {
+//    const clone = template.cloneNode('true');
+//    main.appendChild(clone);
+//}
 
-console.log("da");
-for (let i = 1; i < 4; i++) {
-    const clone = template.cloneNode('true');
-//    clone.querySelector('h2').textContent += i;
+fetch(link).then(result => result.json()).then(data => {
+    show(data["core-areas"])
+});
+
+function show(jData) {
+
+Object.keys(jData).forEach(function(elem){
+   console.log(elem + '=' + jData[elem]);
+    elem = jData[elem];
+    const clone = template.cloneNode(true);
+    clone.querySelector('.name').textContent += elem.name;
+    clone.querySelector('.about').textContent += elem.about;
+    clone.querySelector('.knowledge').textContent += elem.knowledge;
+    clone.querySelector('.skills').textContent += elem.skills;
+    clone.querySelector('.competencies').textContent += elem.competencies;
     main.appendChild(clone);
-}
-//fetch(link).then(result => result.json()).then(data => {
-//    console.log(data);
-//    show(data)
-//});
-
-
-function show(data) {
-    data.forEach(elem => {
-        console.log(elem.shortdescription);
-        const clone = template.cloneNode(true);
-        clone.querySelector('.about').textContent += elem.about;
-        clone.querySelector('.knowledge').textContent += elem.knowledge;
-        clone.querySelector('.skills').textContent += elem.skills;
-        clone.querySelector('.competencies').textContent += elem.competencies;
-        main.appendChild(clone);
-    })
-
+    
+});
 }
