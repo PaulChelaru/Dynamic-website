9ba0c0ee6adb21efc3a07b131ecdec666615c1d9
Paul Chelaru <paul5175@stud.kea.dk>
2018-02-22 17:07:31 +0100

    Revert "Cat de cat prima pagina"
    This reverts commit ffef5071a3647efb1d90430fed53e661fdfa32e7.

--- java.js
+++ java.js
@@ -3,6 +3,7 @@
 
 const template = document.querySelector('template').content;
 const main = document.querySelector('main');
+<<<<<<< HEAD
 const link = "https://raw.githubusercontent.com/PaulChelaru/Dynamic-website/master/data.json?token=AeqwT8ebbeO9NuqQkaUh5pE9Sa2YtPqjks5alwsxwA%3D%3D";
 
 //for (let i = 1; i < 4; i++) {
@@ -52,7 +53,34 @@ Object.keys(jData).forEach(function(elem){
     
     
     clone.querySelector('.competencies').textContent += elem.competencies;
+=======
+const link = "https://raw.githubusercontent.com/lanasladovic/data/master/data.json";
+
+import data from './data.json';
+console.log(data);
+
+console.log("da");
+for (let i = 1; i < 4; i++) {
+    const clone = template.cloneNode('true');
+//    clone.querySelector('h2').textContent += i;
+>>>>>>> parent of ffef507... Cat de cat prima pagina
     main.appendChild(clone);
-    
-});
+}
+//fetch(link).then(result => result.json()).then(data => {
+//    console.log(data);
+//    show(data)
+//});
+
+
+function show(data) {
+    data.forEach(elem => {
+        console.log(elem.shortdescription);
+        const clone = template.cloneNode(true);
+        clone.querySelector('.about').textContent += elem.about;
+        clone.querySelector('.knowledge').textContent += elem.knowledge;
+        clone.querySelector('.skills').textContent += elem.skills;
+        clone.querySelector('.competencies').textContent += elem.competencies;
+        main.appendChild(clone);
+    })
+
 }
