4b3423a9b4c83c8e7d2e4324412ee597db8ec746
Paul Chelaru <paul5175@stud.kea.dk>
2018-02-22 15:37:09 +0100

    Home page basic

--- java.js
+++ java.js
@@ -22,8 +22,35 @@ Object.keys(jData).forEach(function(elem){
     const clone = template.cloneNode(true);
     clone.querySelector('.name').textContent += elem.name;
     clone.querySelector('.about').textContent += elem.about;
-    clone.querySelector('.knowledge').textContent += elem.knowledge;
+        clone.querySelector('.knowledge').textContent += elem.knowledge;
     clone.querySelector('.skills').textContent += elem.skills;
+    
+    
+    
+    clone.querySelector('.competencies').textContent += elem.competencies;
+    main.appendChild(clone);
+    
+});
+}
+
+
+fetch(link).then(result => result.json()).then(data => {
+    show(data[""])
+});
+
+function show(jData) {
+
+Object.keys(jData).forEach(function(elem){
+   console.log(elem + '=' + jData[elem]);
+    elem = jData[elem];
+    const clone = template.cloneNode(true);
+    clone.querySelector('.name').textContent += elem.name;
+    clone.querySelector('.about').textContent += elem.about;
+        clone.querySelector('.knowledge').textContent += elem.knowledge;
+    clone.querySelector('.skills').textContent += elem.skills;
+    
+    
+    
     clone.querySelector('.competencies').textContent += elem.competencies;
     main.appendChild(clone);
     
